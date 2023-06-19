8b12752b9c51e676cbd55528307d439b09d9b648
SleepiCaffeine <depressed.machiato@gmail.com>
2023-06-20 01:02:14 +0300

    Updated Node Classes

---
+++ Data Structures/double_node.h
@@ -0,0 +1,224 @@
+/**
+ * @file double_node.h
+ * @author Vakaris Michejenko (sleepicaffeine@gmail.com)
+ * @brief  A header that defines a dynamic double_node class
+ * @version 0.2
+ * @date 2023-06-19
+ * @copyright Copyright (c) 2023
+ * @link https://github.com/SleepiCaffeine
+ */
+
+#ifndef DOUBLE_NODE_H
+#define DOUBLE_NODE_H
+
+/*!
+ * @class double_node 
+ * @brief double_node class.
+ * 
+ * @details A Node class, that has a pointer to a node in front and behind. It has all of the functionalities of the node class, however it does NOT inherit it.
+ * 
+ * @fn get_data()
+ * @fn set_data(T dt)
+ * @fn get_next()
+ * @fn set_next(double_node<T>* const nd)
+ * @fn get_prev()
+ * @fn set_prev(double_node<T>* const nd)
+ * @tparam T typename
+ */
+template <typename T>
+class double_node {
+
+protected:
+    double_node<T>* next;
+    double_node<T>* prev;
+    T data;
+
+public:
+    /**
+     * Creates a new double_node that points to NULL in both ways and has NULL data.
+     * @brief Default constructor.
+     * @see double_node(const T dt)
+     * @see double_node(double_node<T>* const nd)
+     * @see double_node(double_node<T>& const nd)
+     * @see double_node(double_node<T>* const nd, const T dt)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+     */
+    double_node() 
+        : next{nullptr}, prev{nullptr} { };
+
+    /**
+     * Creates a new double_node that points to NULL in both ways, and has data.
+     * @brief Constructor.
+     * @param dt double_node data 
+     * @see double_node()
+     * @see double_node(double_node<T>* const nd)
+     * @see double_node(double_node<T>& const nd) 
+     * @see double_node(double_node<T>* const nd, const T dt)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+     */
+    double_node(const T dt)
+        : double_node{nullptr, nullptr, dt} { };
+
+    /**
+     * Creates a new double_node that points to NULL in both ways, and has data.
+     * @brief Constructor.
+     * @param dt double_node data 
+     * @see double_node()
+     * @see double_node(const T dt)
+     * @see double_node(double_node<T>& const nd)  
+     * @see double_node(double_node<T>* const nd, const T dt)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+     */
+    double_node(double_node<T>* const nd)
+        : next{nd}, prev{nullptr} { }
+
+   /**
+    * Creates a new double_node object that points to a double_node, and has data.
+    * @note The parameter node will NOT point back to this node
+    * @brief Constructor.
+    * @param nd double_node to which it will point
+    * @param dt double_node data
+    * @see double_node()
+    * @see double_node(const T dt)
+    * @see double_node(double_node<T>* const nd)
+    * @see double_node(double_node<T>& const nd)
+    * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+    * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+    */
+    double_node(double_node<T>* const nd, const T dt)
+        : next{nd}, data{dt} { };
+
+    /**
+     * Creates a new double_node object that points to 2 double_nodes, but has no data.
+     * @note The 2 nodes provided as parameters will NOT point to this new node. You will have to either do this manually or use a DLL 
+     * 
+     * @param nnd double_node to which it will point
+     * @param bnd double_node which will point to it
+     * @see double_node()
+     * @see double_node(const T dt)
+     * @see double_node(double_node<T>* nd)
+     * @see double_node(double_node<T>& nd)
+     * @see double_node(double_node<T>* const nd, const T dt)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+     */
+    double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+        : next{nnd}, prev{bnd} {}
+
+    
+    /**
+     * Creates a new double_node object that points to 2 double_nodes, and has data
+     * @note The 2 nodes provided as parameters will NOT point to this new node. You will have to either do this manually or use a DLL 
+     * 
+     * @param nnd double_node to which it will point
+     * @param bnd double_node which will point to it
+     * @param dt double_node data 
+     * @see double_node()
+     * @see double_node(const T dt)
+     * @see double_node(double_node<T>*const nd)
+     * @see double_node(double_node<T>& const nd)
+     * @see double_node(double_node<T>* const nd, const T dt)
+     * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+     */
+    double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+        : next{nnd}, prev{bnd}, data{dt}  {}
+    
+/** 
+    * Construct a new double_node object from another double_node object.
+    * @brief Copy constructor.
+    * @param nd double_node object which will be copied
+    * @see double_node()
+    * @see double_node(const T dt)
+    * @see double_node(double_node<T>* nd)
+    * @see double_node(double_node<T>* const nd, const T dt)
+    * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd)
+    * @see double_node(double_node<T>* const nnd, double_node<T>* const bnd, const T dt)
+    */
+    double_node(const double_node<T>& nd);
+
+/**
+    * @brief Get the pointer to the next double_node 
+    * @return double_node object to which the current double_node object points to 
+    * @see set_next(double_node<T>* const nd)
+    */
+    double_node<T>* get_next() const;
+
+    /**
+     * @brief Set the pointer to the next Node
+     * @param *nd double_node object to which the current double_node object will point to
+     * @see get_next()
+     */
+    void set_next(double_node<T>* const nd);
+
+    /**
+     * @brief Get the pointer to the node behind the current double_node
+     * @return double_node object which points to this double_node
+     * @see set_prev(double_node<T>* const nd)
+     */
+    double_node<T>* get_prev() const;
+
+    /**
+     * @brief Set the pointer to the node behind the current double_node
+     * @param *nd double_node object which points to the current double_node object
+     * @see get_prev()
+     */
+    void set_prev(double_node<T>* const nd);
+
+        /**
+     * @brief Get the double node's data
+     * @return double_node data
+     * @see setData(const T dt)
+     */
+    T get_data() const;
+
+    /**
+     * @brief Set the current double node's data
+     * @param dt new double_node data
+     * @see getData() 
+     */
+    void set_data(const T dt);
+};
+
+template <typename T>
+double_node<T>::double_node(const double_node<T>& nd) {
+    next = new double_node<T>();
+    prev = new double_node<T>();
+
+    next = nd.get_next();
+    prev = nd.get_prev();
+    data = nd.get_data();
+}
+
+template <typename T>
+void double_node<T>::set_prev(double_node<T>* const nd) {
+    this->prev = nd;
+}
+
+template <typename T>
+double_node<T>* double_node<T>::get_prev() const {
+    return this->prev;
+}
+
+template <typename T>
+void double_node<T>::set_next(double_node<T>* const nd) {
+    this->next = nd;
+}
+
+template <typename T>
+double_node<T>* double_node<T>::get_next() const {
+    return this->next;
+}
+
+template <typename T>
+T double_node<T>::get_data() const {
+    return this->data;
+}
+
+template <typename T>
+void double_node<T>::set_data(const T dt) {
+    this->data = dt;
+}
+
+#endif // DOUBLE_NODE_H
--- Data Structures/node.h
+++ Data Structures/node.h
@@ -20,17 +20,13 @@
  * @fn set_data(T dt)
  * @fn get_next()
  * @fn set_next(Node<T>* nd)
- * @fn get_prev()
- * @fn set_prev(Node<T>* nd)
  * @fn forward()
- * @fn backward()
  * @tparam T typename
  */
 template <typename T>
-class Node {
+class node {
 private:
-    Node<T>* next;  /**< Pointer to the next Node*/
-    //Node<T>* prev;  /**< Pointer to the previous Node*/
+    node<T>* next;  /**< Pointer to the next Node*/
     T data;         /**< Data which is stored in the Node*/
 
 public:
@@ -38,7 +34,7 @@ public:
      * Creates a new node that points to NULL both ways, but has NULL data.
      * @brief Default constructor.
      */
-    Node()
+    node()
         : next{nullptr}, data{} { }
 
     /**
@@ -46,7 +42,7 @@ public:
      * @brief Constructor.
      * @param dt node data.
      */
-    Node(const T dt)
+    node(const T dt)
         : next{nullptr}, data{dt} { }
 
     /**
@@ -55,7 +51,7 @@ public:
      * @param nd node to which it will point.
      * @param dt node data.
      */
-    Node(Node<T>* const nd, const T dt)
+    node(node<T>* const nd, const T dt)
         : next{nd}, data{dt} { }
 
     /**
@@ -65,7 +61,7 @@ public:
      * @param pnd node to which it points to behind.
      * @param dt node data.
      */
-    Node(Node<T>* const nnd, Node<T>* const pnd, const T dt)
+    node(node<T>* const nnd, node<T>* const pnd, const T dt)
         : next{nnd}, data{dt} { }
 
     /**
@@ -73,7 +69,7 @@ public:
      * @brief Copy constructor.
      * @param nd node to copy from.
      */
-    Node(const Node<T>& nd)
+    node(const node<T>& nd)
         : next{nd.next}, data{nd.data} { }
 
 
@@ -97,7 +93,7 @@ public:
      * @brief Get the pointer to the next node.
      * @return node object to which the current node object points to next.
      */
-    Node<T>* get_next() const {
+    node<T>* get_next() const {
         return this->next;
     }
 
@@ -105,7 +101,7 @@ public:
      * @brief Set the pointer to the next node.
      * @param nd new node object to which the current node object will point to next.
      */
-    void set_next(Node<T>* const nd) {
+    void set_next(node<T>* const nd) {
         this->next = nd;
     }
 
@@ -115,21 +111,9 @@ public:
     void forward() {
         if (this->next) {
             this->data = next->get_data();
-            //this->prev = next->get_prev();
             this->next = next->get_next();
         }
     }
-    
-    /**
-     * @brief Set current node to the previous node.
-     */
-    // void backward() {
-    //     if(this->prev) {
-    //         this->data = next->get_data();
-    //         this->next = next->get_next();
-    //         this->prev = next->get_prev();
-    //     }
-    // }
 };
 
 #endif // NODE_H
