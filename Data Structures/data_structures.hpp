f5304cd6b6bd22af9c009c22b54fb0faf87f7065
SleepiCaffeine <depressed.machiato@gmail.com>
2023-06-24 00:14:27 +0300

    Added stack header and version number

--- Data Structures/data_structures.hpp
+++ Data Structures/data_structures.hpp
@@ -1,3 +1,6 @@
+// ver: 0.3
+
 #include "sl_list.hpp"      // Includes node.hpp, <cstddef>, <stdexcept>     
 #include "dl_list.hpp"      // Includes double_node.hpp, <cstddef>, <stdexcept>
 #include "bst.hpp"
+#include "stack.hpp"
